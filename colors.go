e2770ac52bea1a1422337fea755dc6cb79fe233f
Dreae <penitenttangentt@gmail.com>
2016-05-02 18:20:44 -0400

    Add godocs

--- colors.go
+++ colors.go
@@ -2,90 +2,112 @@ package colors
 
 import "bytes"
 
+// Returns a string wrapped in ANSI start/stop codes for bold
 func Bold(in string) string {
   return stylize("\x1b[1m", in, "\x1b[22m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for italic
 func Italic(in string) string {
   return stylize("\x1b[3m", in, "\x1b[23m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for underline
 func Underline(in string) string {
   return stylize("\x1b[4m", in, "\x1b[24m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes to reverse the foreground and background colors
 func Inverse(in string) string {
   return stylize("\x1b[7m", in, "\x1b[27m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for strikethrough text
 func Strikethrough(in string) string {
   return stylize("\x1b[9m", in, "\x1b[29m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground black
 func Black(in string) string {
   return stylize("\x1b[30m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground red
 func Red(in string) string {
   return stylize("\x1b[31m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground green
 func Green(in string) string {
   return stylize("\x1b[32m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground yellow
 func Yellow(in string) string {
   return stylize("\x1b[33m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground blue
 func Blue(in string) string {
   return stylize("\x1b[34m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground magenta
 func Magenta(in string) string {
   return stylize("\x1b[35m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground cyan
 func Cyan(in string) string {
   return stylize("\x1b[36m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for foreground white
 func White(in string) string {
   return stylize("\x1b[37m", in, "\x1b[39m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background black
 func BgBlack(in string) string {
   return stylize("\x1b[40m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background red
 func BgRed(in string) string {
   return stylize("\x1b[41m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background green
 func BgGreen(in string) string {
   return stylize("\x1b[42m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background yellow
 func BgYellow(in string) string {
   return stylize("\x1b[43m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background blue
 func BgBlue(in string) string {
   return stylize("\x1b[44m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background magenta
 func BgMagenta(in string) string {
   return stylize("\x1b[45m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background cyan
 func BgCyan(in string) string {
   return stylize("\x1b[46m", in, "\x1b[49m")
 }
 
+// Returns a string wrapped in ANSI start/stop codes for background white
 func BgWhite(in string) string {
   return stylize("\x1b[47m", in, "\x1b[49m")
 }
 
+// Wraps string in provided ANSI codes
 func stylize(style string, text string, closing string) string {
   var buffer bytes.Buffer
   buffer.WriteString(style)
---
+++ doc.go
@@ -0,0 +1,18 @@
+/*
+# colors
+Simple go ANSI colors library
+
+# Usage
+
+Simply wrap a string in a call to the function for the desired color:
+  greenText := colors.Green("This is green!")
+
+This will return strings that have been wrapped in ANSI codes to start and stop
+the desired color. Printing these strings to the console will result the
+appropriate colors on supported terminals. 
+
+All colors are also available as background colors:
+  greenBackground := colors.BgGreen("The background is now green!")
+
+*/
+package colors
